1db780988350a0bdc39e0ce4a1e98819d6bfa0c3
uvpatel <uvpatel7271@gmail.com>
2026-04-01 13:24:21 +0530

    make uv

--- models/user_model.py
+++ models/user_model.py
@@ -4,3 +4,8 @@ class User(BaseModel):
     id: int = Field(..., description="User ID")
     name: str = Field(..., description="User name")
     email: str = Field(..., description="User email")
+
+
+uv = User(id=1, name="John Doe", email="john.doe@example.com")
+
+print(uv)
