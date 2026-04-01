1f9c44336f78b04a7df08069b7f3fca24e699bbd
Darshan Ajudiya <darshanajudiya07@gmail.com>
2026-04-01 13:24:22 +0530

    make da

--- models/user_model.py
+++ models/user_model.py
@@ -4,3 +4,6 @@ class User(BaseModel):
     id: int = Field(..., description="User ID")
     name: str = Field(..., description="User name")
     email: str = Field(..., description="User email")
+
+darshan = User(id=1, name="Darshan", email="d7ajudiya@gmail.com")
+print(darshan)
